8acc83d1b466dc2fbf006c8e783b2cbddc08df19
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-11-01 02:28:46 +0900

    WCFサービス作成

---
+++ WCF/NDS38.WCF.Service/IGreetingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace NDS38.WCF.Service
+{
+	[ServiceContract]
+	public interface IGreetingService
+	{
+		[OperationContract]
+		string Greet(string name);
+	}
+}
