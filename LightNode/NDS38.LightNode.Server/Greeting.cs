90acb49e444a7996a7db1689b89197a58af9d91c
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-11-01 03:00:56 +0900

    LightNodeサーバー作成

---
+++ LightNode/NDS38.LightNode.Server/Greeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using LightNode.Server;
+
+namespace NDS38.LightNode.Server
+{
+	public class Greeting: LightNodeContract
+	{
+		public string Greet(string name)
+		{
+			return String.Format("こんにちは、{0}さん", name);
+		}
+	}
+}
---
+++ LightNode/NDS38.LightNode.Server/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Owin;
+using Microsoft.Owin;
+
+using LightNode.Server;
+using LightNode.Formatter;
+
+[assembly: OwinStartup(typeof(NDS38.LightNode.Server.Startup))]
+
+namespace NDS38.LightNode.Server
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.UseLightNode(new LightNodeOptions(
+				AcceptVerbs.Get | AcceptVerbs.Post,
+				new JsonNetContentFormatter()));
+		}
+	}
+}
